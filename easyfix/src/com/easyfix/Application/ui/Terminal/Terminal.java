5853bab29b3609015e0d57622f0e94a15613b056
fahadbashir01 <73698643+fahadbashir01@users.noreply.github.com>
2020-11-03 16:51:55 +0500

    Added login fucntionality

--- easyfix/src/com/easyfix/Application/ui/Terminal/Terminal.java
+++ easyfix/src/com/easyfix/Application/ui/Terminal/Terminal.java
@@ -1,15 +1,27 @@
 package com.easyfix.Application.ui.Terminal;
 
+import com.easyfix.Application.bl.services.CustomerService;
 import com.easyfix.Application.ui.UI;
+import java.util.*;
 
 public class Terminal extends UI {
 
     public Terminal(){
         super();
     }
+
     public void start(){
-        int userid = customerService.login("hamza@gmail.com","12356");
-        System.out.println("Logined User ID: ");
-        System.out.print(userid);
+        String email = "hamza@gmail.com";
+        String password = "12356";
+        int userid;
+        try {
+            userid = customerService.login(email, password);
+            System.out.println("Logined User ID: ");
+            System.out.print(userid);
+        }
+        catch(Exception e) {
+            System.out.println("Error!!!!");
+        }
+
     }
 }
