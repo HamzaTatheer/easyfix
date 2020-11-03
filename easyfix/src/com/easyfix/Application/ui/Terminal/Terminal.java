b9b84bfca54d141f2dbc9e9fd5dcfca5b9819ed1
HamzaTatheer <hamzatatheer@gmail.com>
2020-11-04 00:21:19 +0500

    merged it

--- easyfix/src/com/easyfix/Application/ui/Terminal/Terminal.java
+++ easyfix/src/com/easyfix/Application/ui/Terminal/Terminal.java
@@ -1,5 +1,7 @@
 package com.easyfix.Application.ui.Terminal;
 
+import com.easyfix.Application.models.ChatMessageModel;
+import com.easyfix.Application.models.UserModel;
 import com.easyfix.Application.ui.UI;
 
 import java.util.ArrayList;
@@ -22,10 +24,46 @@ public class Terminal extends UI {
             System.out.println(e.getMessage());
         }
 */
-
-
+        /*
+        showing favourites
         ArrayList<Integer> favourites = customerService.getFavourites(1);
         String output = favourites.toString();
         System.out.println(output);
+        */
+
+/*
+
+        load message between two users through their ids
+        ArrayList<ChatMessageModel> messages =  chatService.loadMessageHistory(1,2);
+        for(ChatMessageModel part: messages) {
+            System.out.println(part.senderName + ": " + part.message);
+        }
+*/
+
+
+/*
+get user name
+        try {
+            UserModel u = userService.getUser(2);
+            System.out.println(u.name);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+*/
+
+
+        /*
+        get user speciality
+        try {
+            String u = userService.getUserSpeciality(1);
+            System.out.println(u);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }*/
+
+
+
     }
 }
--- easyfix/src/com/easyfix/Application/ui/UI.java
+++ easyfix/src/com/easyfix/Application/ui/UI.java
@@ -1,12 +1,19 @@
 package com.easyfix.Application.ui;
 import com.easyfix.Application.bl.serviceProviders;
+import com.easyfix.Application.bl.services.ChatService;
 import com.easyfix.Application.bl.services.CustomerService;
+import com.easyfix.Application.bl.services.UserService;
+import com.easyfix.Application.models.UserModel;
 
 public abstract class UI {
     public CustomerService customerService;
+    public ChatService chatService;
+    public UserService userService;
 
     public UI(){
         customerService = serviceProviders.getCustomerService();
+        chatService = serviceProviders.getChatService();
+        userService = serviceProviders.getUserService();
     }
 
 }
