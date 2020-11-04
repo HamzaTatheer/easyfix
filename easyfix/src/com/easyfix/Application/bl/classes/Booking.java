a65abadbf3cefd18c7c74e52bb07572ea9284a50
ruqqia18 <73605086+ruqqia18@users.noreply.github.com>
2020-11-04 20:13:05 +0500

    Added startTime and endTime in Booking and BookingModel

--- easyfix/src/com/easyfix/Application/bl/classes/Booking.java
+++ easyfix/src/com/easyfix/Application/bl/classes/Booking.java
@@ -1,23 +1,27 @@
 package com.easyfix.Application.bl.classes;
 
 import com.easyfix.Application.models.BookingModel;
-
+import java.time.LocalDateTime;
 public class Booking {
     public int id;
     public int wid;
     public int cid;
     public String text;
     public String status;
+    public LocalDateTime startTime;
+    public LocalDateTime endTime;
     //constructors
     Booking(){
 
     }
-    Booking(int _id,int _wid,int _cid,String _text,String _status){
+    Booking(int _id,int _wid,int _cid,String _text,String _status,LocalDateTime _startTime,LocalDateTime _endTime){
         id=_id;
         wid=_wid;
         cid=_cid;
         text=_text;
         status=_status;
+        startTime=_startTime;
+        endTime=_endTime;
     }
     Booking(BookingModel obj){
         id=obj.id;
@@ -25,6 +29,8 @@ public class Booking {
         cid=obj.cid;
         text=obj.text;
         status=obj.status;
+        startTime=obj.startTime;
+        endTime=obj.endTime;
 
     }
     //setters
@@ -38,6 +44,8 @@ public class Booking {
         temp.cid=obj.cid;
         temp.text=obj.text;
         temp.status=obj.status;
+        temp.startTime=obj.startTime;
+        temp.endTime=obj.endTime;
         return temp;
     }
 }
