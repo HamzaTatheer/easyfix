6edb304d077dc527cfa706ea8c64e9ad5f596c28
ruqqia18 <73605086+ruqqia18@users.noreply.github.com>
2020-11-04 16:52:55 +0500

    Check GUI Desktop

--- easyfix/src/com/easyfix/Application/bl/classes/Billing.java
+++ easyfix/src/com/easyfix/Application/bl/classes/Billing.java
@@ -10,6 +10,8 @@ public class Billing {
 
     //constructors
     Billing(){
+        // checkkkkkkkkkkkkkkkkkkk
+        /////dhwhdfjwhejfhwejhj
 
     }
     //getters
--- easyfix/src/com/easyfix/Application/bl/managers/BillingManager.java
+++ easyfix/src/com/easyfix/Application/bl/managers/BillingManager.java
@@ -1,4 +1,12 @@
 package com.easyfix.Application.bl.managers;
 
+import com.easyfix.Application.bl.classes.Billing;
+import com.easyfix.Application.bl.classes.Booking;
+import com.easyfix.Application.models.BillingModel;
+import java.lang.Math;
 public class BillingManager {
+public BillingModel generate_Bill(Booking obj){
+    BillingModel temp=new BillingModel();
+    return temp;
+}
 }
