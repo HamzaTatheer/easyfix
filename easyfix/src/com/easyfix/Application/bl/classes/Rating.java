b70e3e2a9325b6d4b115ba7e7288608ec1ffa728
Ria <l181134@lhr.nu.edu.pk>
2020-11-04 08:49:14 +0500

    Made changes in Rating.java

--- easyfix/src/com/easyfix/Application/bl/classes/Rating.java
+++ easyfix/src/com/easyfix/Application/bl/classes/Rating.java
@@ -1,4 +1,48 @@
 package com.easyfix.Application.bl.classes;
 
+import com.easyfix.Application.models.RatingModel;
+
 public class Rating {
+    public int cid;
+    public int wid;
+    public int rating;
+    //constructors
+    Rating(){
+
+    }
+    Rating(int _cid,int _wid,int _rating){
+        cid=_cid;
+        wid=_wid;
+        rating=_rating;
+    }
+    Rating(RatingModel R){
+        cid=R.cid;
+        wid=R.wid;
+        rating=R.rating;
+    }
+    //setters
+
+
+    //getters
+
+
+    public int getCid() {
+        return cid;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public int getWid() {
+        return wid;
+    }
+
+    public RatingModel get_RatingModel(){
+        RatingModel temp=new RatingModel();
+        temp.cid=cid;
+        temp.rating=rating;
+        temp.wid=wid;
+        return temp;
+    }
 }
