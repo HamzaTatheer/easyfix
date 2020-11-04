ca512866beae2cbe576f864cfa4821ed1cf5b4d2
Ria <l181134@lhr.nu.edu.pk>
2020-11-04 08:51:49 +0500

    Made changes in Booking.java

--- easyfix/src/com/easyfix/Application/bl/classes/Booking.java
+++ easyfix/src/com/easyfix/Application/bl/classes/Booking.java
@@ -1,4 +1,9 @@
 package com.easyfix.Application.bl.classes;
 
 public class Booking {
+    public int id;
+    public int wid;
+    public int cid;
+    public String text;
+    public String status;
 }
