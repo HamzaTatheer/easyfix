1fa8212db33fdb6464ed4a08e2d25e740ad8aa71
Ria <l181134@lhr.nu.edu.pk>
2020-11-04 07:52:08 +0500

    made changes in models

--- easyfix/src/com/easyfix/Application/models/CustomerModel.java
+++ easyfix/src/com/easyfix/Application/models/CustomerModel.java
@@ -2,11 +2,13 @@ package com.easyfix.Application.models;
 
 import com.easyfix.Application.bl.classes.User;
 
+import java.util.ArrayList;
+
 public class CustomerModel extends UserModel{
     public Float wallet;
     public String creditno;
     public String paymentMethod;
     public String city;
     public String area;
-    public int[] Favourite;
+    public ArrayList<FavouriteModel>Favourites;
 }
--- easyfix/src/com/easyfix/Application/models/FavouriteModel.java
+++ easyfix/src/com/easyfix/Application/models/FavouriteModel.java
@@ -1,4 +1,7 @@
 package com.easyfix.Application.models;
 
+import com.easyfix.Application.bl.classes.Worker;
+
 public class FavouriteModel {
+    public WorkerModel obj;
 }
