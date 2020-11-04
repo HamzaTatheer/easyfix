42a3405aaa4cdd5885d8b8dbe8bbcc13298df1b5
Ria <l181134@lhr.nu.edu.pk>
2020-11-04 07:55:35 +0500

    Made changes in Worker.java

--- easyfix/src/com/easyfix/Application/bl/classes/Worker.java
+++ easyfix/src/com/easyfix/Application/bl/classes/Worker.java
@@ -1,10 +1,11 @@
 package com.easyfix.Application.bl.classes;
 
 import com.easyfix.Application.models.WorkerModel;
-
+import java.io.*;
+import java.util.*;
 public class Worker extends  User{
     public float avgRating;
-    public float HourlyRate;
+    public float hourlyRate;
     public String speciality;
     public String city;
     public String area;
@@ -15,7 +16,7 @@ public class Worker extends  User{
         email =_email;
         password =_password;
         avgRating = avgrating;
-        HourlyRate = _HourlyRate;
+        hourlyRate = _HourlyRate;
         speciality = _speciality;
         city = _city;
         area = _area;
@@ -27,7 +28,7 @@ public class Worker extends  User{
         email =model.email;
         password = model.password;
         avgRating = model.avgRating;
-        HourlyRate = model.hourlyRate;
+        hourlyRate = model.hourlyRate;
         speciality = model.speciality;
         city = model.city;
         area = model.area;
@@ -40,7 +41,7 @@ public class Worker extends  User{
         w.name = name;
         w.email =email;
         w.password =password;
-        w.hourlyRate = HourlyRate;
+        w.hourlyRate = hourlyRate;
         w.speciality = speciality;
         w.city = city;
         w.area = area;
@@ -51,7 +52,7 @@ public class Worker extends  User{
 
     public Boolean changeHourlyRate(float new_rate){
         if(new_rate >= 0) {
-            HourlyRate = new_rate;
+            hourlyRate = new_rate;
             return true;
         }
         return false;
