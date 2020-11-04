4e2e628bf74e0de52bb9875f59d72ad1f9c9fb71
Ria <l181134@lhr.nu.edu.pk>
2020-11-04 08:19:42 +0500

    Made changes in User.java

--- easyfix/src/com/easyfix/Application/bl/classes/User.java
+++ easyfix/src/com/easyfix/Application/bl/classes/User.java
@@ -1,4 +1,6 @@
 package com.easyfix.Application.bl.classes;
+import com.easyfix.Application.models.UserModel;
+
 import java.io.*;
 import java.util.*;
 public class User {
@@ -7,18 +9,48 @@ public class User {
     public String email;
     public String password;
 
+    //constructors
+    User(){
+
+    }
+    User(int _id,String _name,String _email,String _password){
+        id=_id;
+        name=_name;
+        email=_email;
+        password=_password;
+    }
+    User(UserModel U){
+        id=U.id;
+        name=U.name;
+        email=U.email;
+        password=U.password;
+    }
+    //Setters
     public Boolean changePassword(String newPassword){
         password = newPassword;
         return true;
     }
-    public User(){
-
-    }
-
+    //getters
     public String getName(){
         return name;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public String getPassword() {
+        return password;
+    }
+    public UserModel get_User_Model(UserModel U){
+        UserModel temp=new UserModel();
+        temp.name=U.name;
+        temp.email=U.email;
+        temp.password=U.password;
+        return  temp;
+    }
 }
