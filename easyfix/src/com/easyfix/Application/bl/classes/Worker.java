dabee260462569623ef915e9bbbf178bf473803a
Ria <l181134@lhr.nu.edu.pk>
2020-11-04 08:07:19 +0500

    Made changes in Worker.java

--- easyfix/src/com/easyfix/Application/bl/classes/Worker.java
+++ easyfix/src/com/easyfix/Application/bl/classes/Worker.java
@@ -10,21 +10,22 @@ public class Worker extends  User{
     public String city;
     public String area;
 
+    //constructors
     Worker(){
 
     }
-    Worker(int _id,String _name,String _email,String _password,String _city,String _area,float avgrating, float _HourlyRate,String _speciality,String _location){
+    Worker(int _id,String _name,String _email,String _password,String _city,String _area,float _avgrating, float _HourlyRate,String _speciality,String _location){
         id = _id;
         name = _name;
         email =_email;
         password =_password;
-        avgRating = avgrating;
+        avgRating = _avgrating;
         hourlyRate = _HourlyRate;
         speciality = _speciality;
         city = _city;
         area = _area;
     }
-
+    // WorkerModel related functions
     Worker(WorkerModel model){
         id = model.id;
         name = model.name;
@@ -36,7 +37,6 @@ public class Worker extends  User{
         city = model.city;
         area = model.area;
     }
-
     public WorkerModel getWorkerModel(){
 
         WorkerModel w = new WorkerModel();
@@ -50,9 +50,7 @@ public class Worker extends  User{
         w.area = area;
         return w;
     }
-
-
-
+    // Change data functions
     public Boolean changeHourlyRate(float new_rate){
         if(new_rate >= 0) {
             hourlyRate = new_rate;
@@ -69,5 +67,48 @@ public class Worker extends  User{
     public Boolean changeCity(String newCity){
         city = newCity;
         return true;
+    }
+    //getters
+
+    public float getAvgRating() {
+        return avgRating;
+    }
+
+    public float getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    //setters
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public void setAvgRating(float avgRating) {
+        this.avgRating = avgRating;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setHourlyRate(float hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
     }
 }
