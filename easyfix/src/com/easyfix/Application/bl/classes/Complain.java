5a035f7f168539f0beb84b31aba5faa5440bd25f
Ria <l181134@lhr.nu.edu.pk>
2020-11-04 08:50:40 +0500

    Made changes in Complain.java

--- easyfix/src/com/easyfix/Application/bl/classes/Complain.java
+++ easyfix/src/com/easyfix/Application/bl/classes/Complain.java
@@ -1,4 +1,37 @@
 package com.easyfix.Application.bl.classes;
 
+import com.easyfix.Application.models.ComplainModel;
+
 public class Complain {
+    public int id;
+    public int wid;
+    public int cid;
+    public String text;
+    //constructors
+    Complain(){
+
+    }
+    Complain(int _id,int _wid,int _cid,String _text){
+        id=_id;
+        wid=_wid;
+        cid=_cid;
+        text=_text;
+    }
+    Complain(ComplainModel C){
+        id=C.id;
+        wid=C.wid;
+        cid=C.cid;
+        text=C.text;
+    }
+    //setters
+
+    //getters
+    ComplainModel get_ComplainModel(){
+        ComplainModel temp=new ComplainModel();
+        temp.cid=cid;
+        temp.id=id;
+        temp.text=text;
+        temp.wid=wid;
+        return temp;
+    }
 }
