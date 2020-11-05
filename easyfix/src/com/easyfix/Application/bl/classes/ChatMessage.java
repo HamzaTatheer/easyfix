738ecfbc880196caa37e79a1a71a32eb3c23de08
ruqqia18 <73605086+ruqqia18@users.noreply.github.com>
2020-11-05 21:47:30 +0500

    Made changes

--- easyfix/src/com/easyfix/Application/bl/classes/ChatMessage.java
+++ easyfix/src/com/easyfix/Application/bl/classes/ChatMessage.java
@@ -5,29 +5,28 @@ import com.easyfix.Application.models.ChatMessageModel;
 public class ChatMessage {
     public int senderId;
     public int receiverId;
-    public String senderName;
-    public String receiverName;
+    //public String senderName;
+    //public String receiverName;
     public String message;
 
     //Constructors
-    ChatMessage(){
-
-    }
-    ChatMessage(int _senderId,int _receiverId,String _senderName,String _receiverName,String _message){
-        senderId = _senderId;
-        receiverId = _receiverId;
-        senderName=_senderName;
-        receiverName=_receiverName;
-        message = _message;
-    }
     ChatMessage(ChatMessageModel model){
         senderId = model.senderId;
         receiverId = model.receiverId;
-        senderName=model.senderName;
-        receiverName=model.receiverName;
+        //senderName=model.senderName;
+        //receiverName=model.receiverName;
         message = model.message;
     }
-
+    //getters
+    public ChatMessageModel getChatMessageModel(){
+        ChatMessageModel temp=new ChatMessageModel();
+        temp.senderId=senderId;
+        temp.receiverId=receiverId;
+        //temp.senderName=senderName;
+        //temp.receiverName=receiverName;
+        temp.message=message;
+        return temp;
+    }
     //setters
     public void hideAbusiveWords(){
         String filteredMessage= message.replaceAll("pagal","HIDDEN");
@@ -36,24 +35,5 @@ public class ChatMessage {
         message = filteredMessage;
     }
 
-    //getters
-    public ChatMessage getChatMessage(ChatMessageModel obj){ //convert model to class
-        ChatMessage temp=new ChatMessage();
-        temp.senderId=obj.senderId;
-        temp.receiverId=obj.receiverId;
-        temp.senderName=obj.senderName;
-        temp.receiverName=obj.receiverName;
-        temp.message=obj.message;
-        return temp;
-    }
-    public ChatMessageModel getChatMessageModel(ChatMessage obj){ //convert class to model
-        ChatMessageModel temp=new ChatMessageModel();
-        temp.senderId=obj.senderId;
-        temp.receiverId=obj.receiverId;
-        temp.senderName=obj.senderName;
-        temp.receiverName=obj.receiverName;
-        temp.message=obj.message;
-        return temp;
-    }
 
 }
