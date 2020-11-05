ebbd4ac57f4540415a47b1d1fe2b96e0369e3041
ArslaanAsad <l180960@lhr.nu.edu.pk>
2020-11-05 11:28:21 +0500

    DB interface removed jason

--- Database/DB_interface.java
+++ Database/DB_interface.java
@@ -1,9 +1,11 @@
 package Classes;
 
-import org.json.simple.JSONObject;
+//import org.json.simple.JSONObject;
+import java.lang.reflect.Array;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
-
+import models.*;
 
 public interface DB_interface {
 
@@ -22,41 +24,36 @@ public ArrayList<WorkerModel> get_worker(String city,String area);
 public ArrayList<WorkerModel> get_all_worker();
 public boolean update_Worker_city(int id,String city);
 public boolean update_Worker_area(int id,String area);
-public boolean update_hourly_rate(int id,int rate);
+public boolean update_hourly_rate(int id,float rate);
 
-public boolean store_booking(int bid, String name, int customer_id, int worker_id, int hours);
+public boolean store_booking(int bid, int customer_id, int worker_id, String text, String status, LocalDateTime start_time,LocalDateTime end_time);
 public  ArrayList<BookingModel> get_booking(int customer_id);
 
 public boolean store_spare_holder(int booking_id,int spare_id,int quantity);
 public ArrayList<SparePartModel> get_all_spare_parts_booking(int booking_id);//part id,quantity
 
 
-public boolean store_worker(int id,int average_rating,int hourly_rate,String city,String area,String speciality,ArrayList rating);
-public JSONObject get_worker(int id);
-public boolean update_Worker_location(int id,String location);
-public boolean update_hourly_rate(int id,int rate);
 
-public boolean store_booking(int bid, String name, int customer_id, int worker_id, Date start_time, Date end_time);
-public  ArrayList<JSONObject> get_booking(int customer_id);
+public boolean store_favourite(int customer_id,ArrayList<Integer> worker_id);
+public ArrayList<Integer> get_favourites(int customer_id);
 
-public boolean store_favourite(int customer_id,ArrayList worker_id);
-public ArrayList get_favourites(int customer_id);
 
-public boolean store_customer_billing(int billing_id,int customer_id,int worker_id,int cost);
-public ArrayList<JSONObject> get_customer_billing(int customer_id);
+public boolean store_customer_billing(int billing_id,int booking_id,int worker_id,int cost);
+public ArrayList<BillingModel> get_customer_billing(int customer_id);
 
 public boolean store_complaint(int complain_id,int customer_id,int worker_id,String complain_text);
-public  JSONObject get_complaint(int complaint_id);
-public ArrayList<JSONObject> show_all_complaint(int customer_id);
+public  ComplainModel get_complaint(int complaint_id);
+public ArrayList<ComplainModel> show_all_complaint(int customer_id);
 
 public boolean store_spar_parts(int spare_id,String name,int cost,int quantity);
-public  JSONObject get_spare_parts(int part_id);
-public ArrayList<JSONObject> get_all_parts();//for quantity>0
+public  SparePartModel get_spare_part(int part_id);
+public ArrayList<SparePartModel> get_all_parts();//for quantity>0
+public boolean deduct_part(int spare_id,int quantity);
 
 public boolean store_rating(int customer_id,int worker_id,int rating);
 public float get_avg_rating(int customer_id);
 
 public  boolean store_chat(int customer_id,int worker_id,String text);
-public ArrayList<JSONObject> get_chat_history(int customer_id,int worker_id);
+public ArrayList<ChatMessageModel> get_chat_history(int customer_id,int worker_id);
 
 }
