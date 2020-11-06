4aafe4cf6d34ab5ff87d9e1acd75d6e736f3513c
ruqqia18 <73605086+ruqqia18@users.noreply.github.com>
2020-11-06 08:07:36 +0500

    Made Changes

--- easyfix/src/com/easyfix/Application/bl/managers/RatingManager.java
+++ easyfix/src/com/easyfix/Application/bl/managers/RatingManager.java
@@ -1,4 +1,23 @@
 package com.easyfix.Application.bl.managers;
 
+import com.easyfix.Application.db.dbProviders;
+import com.easyfix.Application.db.services.CustomerDbService;
+import com.easyfix.Application.db.services.WorkerDbService;
+import com.easyfix.Application.models.RatingModel;
+
+import java.util.ArrayList;
+
 public class RatingManager {
+    //public ArrayList<RatingModel> showAllRatings(int id){ //pending}
+    public void giveRating(int giverID,int receiverID,int workerRating) throws Exception{
+
+        CustomerDbService custdbservice = dbProviders.getCustomerDbService();
+        WorkerDbService workDbService=dbProviders.getWorkerDbService();
+        if((custdbservice.does_customer_exist(giverID)&& (workDbService.does_Worker_Exist(receiverID)))){
+
+        }
+        else {
+            throw new Exception("One of the user doesn't exist. Please try again");
+        }
+    }
 }
