fcf54dc97e1c8cbfbf004f1934b758abc92ae301
ArslaanAsad <l180960@lhr.nu.edu.pk>
2020-11-06 15:18:00 +0500

    billing functions added plus billing model changed

--- Database/DB_interface.java
+++ Database/DB_interface.java
@@ -35,18 +35,19 @@ public boolean update_booking_status(int booking_id,String status);
 //these not needed maybe see them
 public boolean store_spare_holder(int booking_id,int spare_id,int quantity);
 public ArrayList<SparePartModel> get_all_spare_parts_booking(int booking_id);//part id,quantity returned
-
+//------------------------------------
 
 
 public boolean add_favourite(int customer_id,int worker_id);
 public boolean remove_favourite(int customer_id,int worker_id);
 //this not needed
 public ArrayList<Integer> get_favourites(int customer_id);
+//----------------------------------------
 
 
-
-public boolean store_customer_billing(int booking_id,int worker_id,int cost);//int billing_id, given by DB
-public ArrayList<BillingModel> get_customer_billing(int booking_id);
+public boolean store_customer_billing(int booking_id,String title,String customerName,String workerName,String status,Float totalCost);//int billing_id, given by DB
+public  BillingModel get_bill(int booking_id);
+public boolean change_billing_status(int booking_id,String status);
 
 public boolean store_complaint(int customer_id,int worker_id,String complain_text);//int complain_id, given by DB
 public  ComplainModel get_complaint(int complaint_id);
