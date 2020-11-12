27e916bde418464d92811f9ee7ae9e3c5d104b09
fahadbashir01 <73698643+fahadbashir01@users.noreply.github.com>
2020-11-13 02:54:14 +0500

    Added Edit Profile in GUI

--- easyfix/src/com/easyfix/Application/ui/Gui/Controller.java
+++ easyfix/src/com/easyfix/Application/ui/Gui/Controller.java
@@ -49,9 +49,9 @@ public class Controller extends UI{
     @FXML
     private Label label;
     @FXML
-    private Button signup,register,login,next;
+    private Button signup,register,login,next,editprofile,home,changecity,gotochangecity;
     @FXML
-    private TextField name,email,pass,city,area,choice;
+    private TextField name,email,pass,city,area,choice,getcity;
 
     String Choice;
 
@@ -74,49 +74,24 @@ public class Controller extends UI{
             System.out.println(e.getMessage());
         }
         if(userid>0 || workerid>0){
-            Parent root;
-            Stage stage;
-
-            stage = (Stage) login.getScene().getWindow();
-            root = FXMLLoader.load(getClass().getResource("homepage.fxml"));
-
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
+            changeScene("homepage.fxml",login);
         }
     }
     @FXML
     private void handleSignupAction(ActionEvent e) throws Exception{
-        Parent root;
-        Stage stage;
-
-            stage = (Stage) signup.getScene().getWindow();
-            root = FXMLLoader.load(getClass().getResource("signup.fxml"));
-
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        changeScene("signup.fxml",signup);
     }
 
     @FXML
     private void handleRegisterAction(ActionEvent event) throws Exception{
-        int cid=0;
         try {
-            cid = customerService.register(name.getText(),email.getText(),pass.getText(),"123",city.getText(),area.getText());
+            userid = customerService.register(name.getText(),email.getText(),pass.getText(),"123",city.getText(),area.getText());
             System.out.println("Signup successful");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        if(cid>0){
-            Parent root;
-            Stage stage;
-
-            stage = (Stage) register.getScene().getWindow();
-            root = FXMLLoader.load(getClass().getResource("homepage.fxml"));
-
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
+        if(userid>0){
+            changeScene("homepage.fxml",register);
         }
 
     }
@@ -124,21 +99,42 @@ public class Controller extends UI{
         //Display the message
         Choice=message;
     }
-  /*  @FXML
-    private void handleMainAction(ActionEvent event) throws Exception{
-        Choice=choice.getText();
-        System.out.println(Choice);
+    @FXML
+    private void handleHomeAction(ActionEvent e) throws Exception{
+        changeScene("home.fxml",home);
+    }
+
+    @FXML
+    private void handleEditProfileAction(ActionEvent event) throws Exception{
+
+        changeScene("profile.fxml",editprofile);
+
+    }
+    public void changeScene(String file,Button btn)throws Exception{
         Parent root;
         Stage stage;
 
-        stage = (Stage) next.getScene().getWindow();
-        root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        stage = (Stage) btn.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource(file));
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+    @FXML
+    private void handlegotoChangeCityAction(ActionEvent event) throws Exception{
 
-    }*/
+        changeScene("changecity.fxml",gotochangecity);
+
+    }
+    @FXML
+    private void handleChangeCityAction(ActionEvent e) throws Exception{
+        System.out.println("id "+userid+getcity.getText());
+            boolean change = customerService.changeCity(userid, getcity.getText());
+            if (change == true)
+                System.out.println("City Changed Successfully");
+            changeScene("profile.fxml", changecity);
+    }
     //@Override
     public void initialize(URL url, ResourceBundle resources) {
         // Initialization code can go here.
