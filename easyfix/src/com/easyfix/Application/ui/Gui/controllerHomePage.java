46fc88eaf9be0d76e552429abc26f845c04a855b
ruqqia18 <73605086+ruqqia18@users.noreply.github.com>
2020-11-13 04:08:40 +0500

    Made Changes

--- easyfix/src/com/easyfix/Application/ui/Gui/controllerHomePage.java
+++ easyfix/src/com/easyfix/Application/ui/Gui/controllerHomePage.java
@@ -1,15 +1,17 @@
 package com.easyfix.Application.ui.Gui;
 
+import com.easyfix.Application.models.WorkerModel;
 import com.easyfix.Application.ui.UI;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class controllerHomePage extends UI {
     private int Cust_id;
@@ -35,13 +37,23 @@ public class controllerHomePage extends UI {
     public void handleBookWorkerAction(ActionEvent event) throws IOException {
         //System.out.println("Customer Id " + Cust_id+"\n"); //Cust_id passed to Book Worker
         try {
-            Parent root;
-            Stage stage;
-            stage = (Stage) BookWorker.getScene().getWindow();
-            root = FXMLLoader.load(getClass().getResource("BookWorkerXML.fxml"));
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
+            try {
+                ArrayList<WorkerModel> getWork = customerService.getWorkersCloseBy(Cust_id);//get all workers by cust_id
+                for (int i=0;i<getWork.size();i++){
+                    
+                }
+                Parent root;
+                Stage stage;
+                stage = (Stage) BookWorker.getScene().getWindow();
+                root = FXMLLoader.load(getClass().getResource("BookWorkerXML.fxml"));
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }
+            catch (Exception E){
+                E.getMessage();
+            }
+
         }
         catch (Exception E){
             E.getMessage();
@@ -76,4 +88,5 @@ public class controllerHomePage extends UI {
         //Display the message
         Cust_id=_Cust_id;
     }
+
 }
