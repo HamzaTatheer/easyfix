10253674a0fd6be03f647b04e55c5f0319293fe5
HamzaTatheer <hamzatatheer@gmail.com>
2020-11-15 01:35:09 +0500

    rating can only be given once now

--- easyfix/src/com/easyfix/Application/bl/managers/RatingManager.java
+++ easyfix/src/com/easyfix/Application/bl/managers/RatingManager.java
@@ -8,6 +8,7 @@ import com.easyfix.Application.db.dbProviders;
 import com.easyfix.Application.db.services.DbService;
 import com.easyfix.Application.models.BookingModel;
 import com.easyfix.Application.models.RatingModel;
+import com.easyfix.Application.utils.Arrayfuncs;
 
 import java.util.ArrayList;
 
@@ -24,10 +25,32 @@ public class RatingManager implements RatingService {
         }
 
         if(exists = true){
+
+            boolean hasAlreadyGivenRating = false;
+
+            ArrayList<RatingModel> ratings = dbService.getAllRatings(giverID);
+            for (int i=0;i<ratings.size();i++){
+                if(ratings.get(i).wid == receiverID)
+                    hasAlreadyGivenRating = true;
+            }
+
+            if(hasAlreadyGivenRating)
+                throw new Exception("You have already given rating to this worker");
+
             ArrayList<BookingModel> b = dbService.get_booking_of_customer(giverID,"finished");
 
             if(b ==null || b.size()<=0){
                 throw new Exception("Customer needs to have at least one booking with worker before giving rating");
+            }
+
+            boolean bookedWith = false;
+            for (int i=0;i<b.size();i++){
+                if(b.get(i).wid == receiverID)
+                    bookedWith = true;
+            }
+
+            if(bookedWith==false){
+                throw new Exception("You need to book atleast one appointment with this worker");
             }
 
             if(Worker.isRatingCorrect(workerRating)){
