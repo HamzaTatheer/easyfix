5a6c9546e2e1bd04bb9fcb73d2a729a89d9bd3ec
noor14211 <l181236@lhr.nu.edu.pk>
2020-11-15 18:55:52 +0500

    payment method

--- easyfix/src/com/easyfix/Application/db/sqlDb/SqlDbManager.java
+++ easyfix/src/com/easyfix/Application/db/sqlDb/SqlDbManager.java
@@ -297,6 +297,7 @@ public class SqlDbManager implements DbService {
                 c1.wallet=rs.getFloat("wallet");
                 c1.city=rs.getString("city");
                 c1.area=rs.getString("area");
+                c1.paymentMethod=rs.getString("credit_no");
 
             }
 
